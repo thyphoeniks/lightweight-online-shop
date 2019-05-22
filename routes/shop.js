aa70d5b4d0338d1b4a150022d74ba0a319c4a382
thyphoeniks <thyphoeniks@gmail.com>
2019-05-22 16:27:41 +0100

    Template engines, experimenting with PUG

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,15 @@ const express = require("express");
 
 const rootDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log("shop.js", adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
