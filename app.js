21190f10402b4f3c33ea58507f4ade95461ce3ee
thyphoeniks <thyphoeniks@gmail.com>
2019-05-23 11:46:13 +0100

    MVC pattern. Adding controllers and models

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
-/*app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);*/
-// app.set("view engine", "hbs");
-//app.set("view engine", "pug");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res
-    .status(404)
-    .render("404", { pageTitle: "Page Not Found", path: "/error" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,24 +1,13 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
 
 const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  // console.log("shop.js", adminData.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hadProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
